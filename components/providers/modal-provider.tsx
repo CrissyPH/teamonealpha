38245a99197bdb874272bf487d71e16264ed3eb6
Crisanto C. Reyes <ccreyes@onealpha.local>
2023-09-02 10:57:07 +0800

    first commit

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import { CreateServerModal } from "@/components/modals/create-server-modals"
+
+
+import { useEffect, useState } from "react"
+import { InviteModal } from "../modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "../modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteChannelModal } from "../modals/delete-channel-modal";
+import { EditChannelModal } from "../modals/edit-channel.modal";
+import { MessageFileModal } from "../modals/message-file-modal";
+import { DeleteMessageModal } from "../modals/delete-message.modal";
+
+
+export const ModalProvider = () => {
+
+    const [ isMounted, setIsMounted ] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    if (!isMounted) {
+        return null;
+    }
+
+
+    return (
+        <>
+            <CreateServerModal />
+            <InviteModal />
+            <EditServerModal />
+            <MembersModal />
+            <CreateChannelModal />
+            <LeaveServerModal />
+            <DeleteServerModal />
+            <DeleteChannelModal />
+            <EditChannelModal />
+            <MessageFileModal />
+            <DeleteMessageModal />
+        </>
+    )
+}
